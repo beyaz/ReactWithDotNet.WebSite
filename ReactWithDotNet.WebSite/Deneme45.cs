3613431484241aa554634f75207dbda224af0c81
beyaz <beyaz1404@gmail.com>
2024-12-28 22:31:59 +0300

    .

--- ReactWithDotNet.WebSite/Deneme45.cs
+++ ReactWithDotNet.WebSite/Deneme45.cs
@@ -63,7 +63,7 @@ class Test45
                 console.time("ALL_TIME");
             }
 
-            SnakeGame.Start();
+            // SnakeGame.Start();
 
             TupleTests();
             
@@ -655,6 +655,11 @@ class Deneme46
             }
         
             if (arr[2] != 9)
+            {
+                console.log("fail");
+            }
+            
+            if (arr[0] != 0)
             {
                 console.log("fail");
             }
