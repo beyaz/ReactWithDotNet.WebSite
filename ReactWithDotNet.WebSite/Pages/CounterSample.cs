e597dbaeeb115f9e8b691acfad21cce3a901fe56
beyaz <beyaz1404@gmail.com>
2025-01-18 17:55:17 +0300

    .

---
+++ ReactWithDotNet.WebSite/Pages/CounterSample.cs
@@ -0,0 +1,24 @@
+﻿namespace ReactWithDotNet.WebSite.Pages;
+
+class CounterSample: Component<CounterSample.State>
+{
+    protected override Element render()
+    {
+        return new div
+        {
+            new div { "Current count: ", state.Count.ToString() },
+            new button { "Increment", OnClick(OnIncrement) }
+        };
+    }
+
+    Task OnIncrement(MouseEvent e)
+    {
+        state.Count++;
+        return Task.CompletedTask;
+    }
+
+    internal class State
+    {
+        public int Count { get; set; }
+    }
+}
