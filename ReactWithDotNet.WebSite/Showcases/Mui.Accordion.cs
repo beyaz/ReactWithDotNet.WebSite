b8f37cfd788b1f5d4fe85590848e8e1ad1becbce
beyaz <beyaz1404@gmail.com>
2025-02-01 23:00:15 +0300

    .

---
+++ ReactWithDotNet.WebSite/Showcases/Mui.Accordion.cs
@@ -0,0 +1,33 @@
+﻿using ReactWithDotNet.ThirdPartyLibraries.MUI.Material;
+
+namespace ReactWithDotNet.WebSite.Showcases;
+
+class MuiAccordionDemo : PureComponent
+{
+    protected override Element render()
+    {
+        return new FlexRowCentered
+        {
+            new Accordion
+            {
+                
+                Accordion.DefaultExpanded(true),
+                new AccordionSummary
+                {
+                    AccordionSummary.ExpandIcon(new ExpandMoreIcon()),
+                    new Typography { children = { "Accordion 1" } }
+                },
+                
+                new AccordionDetails
+                {
+                    """
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                           malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    """
+                }
+            }
+            
+            
+        };
+    }
+}
