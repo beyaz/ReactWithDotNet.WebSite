abdbbf444602c271494cbf26f389bbd429878085
beyaz <beyaz1404@gmail.com>
2025-02-02 10:33:20 +0300

    .

---
+++ ReactWithDotNet.WebSite/Showcases/MuiAccordionDemo.cs
@@ -0,0 +1,58 @@
+﻿using ReactWithDotNet.ThirdPartyLibraries.MUI.Material;
+
+namespace ReactWithDotNet.WebSite.Showcases;
+
+class MuiAccordionDemo : Component<MuiAccordionDemo.State>
+{
+    protected override Element render()
+    {
+        return new FlexColumnCentered
+        {
+            new div
+            {
+                state.IsExpanded is true ? "Expanded" : "Collapsed"
+            },
+            new Accordion
+            {
+                defaultExpanded = true,
+
+                onChange = OnChange,
+
+                children =
+                {
+                    new AccordionSummary
+                    {
+                        expandIcon = new ExpandMoreIcon(),
+                        children =
+                        {
+                            new Typography
+                            {
+                                "Accordion 1"
+                            }
+                        }
+                    },
+
+                    new AccordionDetails
+                    {
+                        """
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                               malesuada lacus ex, sit amet blandit leo lobortis eget.
+                        """
+                    }
+                }
+            }
+        };
+    }
+
+    Task OnChange(MouseEvent arg1, bool? expanded)
+    {
+        state.IsExpanded = expanded;
+
+        return Task.CompletedTask;
+    }
+
+    internal class State
+    {
+        public bool? IsExpanded { get; set; }
+    }
+}
--- ReactWithDotNet.WebSite/Showcases/MuiCardDemo.cs
+++ ReactWithDotNet.WebSite/Showcases/MuiCardDemo.cs
@@ -43,24 +43,6 @@ class MuiCardDemo : PureComponent
                         new Button { size = "small", children = { "Learn More" } }
                     }
                 }
-            },
-            
-            new Accordion
-            {
-                Accordion.DefaultExpanded(true),
-                new AccordionSummary
-                {
-                    AccordionSummary.ExpandIcon(new ExpandMoreIcon()),
-                    new Typography { children = { "Accordion 1" } }
-                },
-                
-                new AccordionDetails
-                {
-                    """
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                           malesuada lacus ex, sit amet blandit leo lobortis eget.
-                    """
-                }
             }
         };
     }
