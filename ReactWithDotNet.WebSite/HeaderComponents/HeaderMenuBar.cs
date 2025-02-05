d7c746bf69a1f2ac44c6ae2ac8f7424a0bfd6e2a
beyaz <beyaz1404@gmail.com>
2025-02-05 23:46:10 +0300

    .

--- ReactWithDotNet.WebSite/HeaderComponents/HeaderMenuBar.cs
+++ ReactWithDotNet.WebSite/HeaderComponents/HeaderMenuBar.cs
@@ -63,48 +63,21 @@ sealed class HeaderMenuBar : PureComponent
     {
         protected override Element render()
         {
-            var socialMediaLinks = new[]
+            return new FlexRow(Gap(15), AlignItemsCenter, MarginRight(50), WhenMediaMaxWidth(MD, DisplayNone))
             {
-                new
+                new SocialMediaLink
                 {
                     Text = "GitHub",
                     Link = "https://github.com/beyaz/ReactWithDotNet",
                     Svg  = "M12 1.27a11 11 0 00-3.48 21.46c.55.09.73-.28.73-.55v-1.84c-3.03.64-3.67-1.46-3.67-1.46-.55-1.29-1.28-1.65-1.28-1.65-.92-.65.1-.65.1-.65 1.1 0 1.73 1.1 1.73 1.1.92 1.65 2.57 1.2 3.21.92a2 2 0 01.64-1.47c-2.47-.27-5.04-1.19-5.04-5.5 0-1.1.46-2.1 1.2-2.84a3.76 3.76 0 010-2.93s.91-.28 3.11 1.1c1.8-.49 3.7-.49 5.5 0 2.1-1.38 3.02-1.1 3.02-1.1a3.76 3.76 0 010 2.93c.83.74 1.2 1.74 1.2 2.94 0 4.21-2.57 5.13-5.04 5.4.45.37.82.92.82 2.02v3.03c0 .27.1.64.73.55A11 11 0 0012 1.27"
                 },
                 
-                new
+                new SocialMediaLink
                 {
                     Text = "Twitter",
                     Link = "https://x.com/ReactWithDotNet",
                     Svg  = "M19.633 7.997c.013.175.013.349.013.523 0 5.325-4.053 11.461-11.46 11.461-2.282 0-4.402-.661-6.186-1.809.324.037.636.05.973.05a8.07 8.07 0 0 0 5.001-1.721 4.036 4.036 0 0 1-3.767-2.793c.249.037.499.062.761.062.361 0 .724-.05 1.061-.137a4.027 4.027 0 0 1-3.23-3.953v-.05c.537.299 1.16.486 1.82.511a4.022 4.022 0 0 1-1.796-3.354c0-.748.199-1.434.548-2.032a11.457 11.457 0 0 0 8.306 4.215c-.062-.3-.1-.611-.1-.923a4.026 4.026 0 0 1 4.028-4.028c1.16 0 2.207.486 2.943 1.272a7.957 7.957 0 0 0 2.556-.973 4.02 4.02 0 0 1-1.771 2.22 8.073 8.073 0 0 0 2.319-.624 8.645 8.645 0 0 1-2.019 2.083z"
                 }
-            };
-
-            return new FlexRow(Gap(15), AlignItemsCenter, MarginRight(50), WhenMediaMaxWidth(MD, DisplayNone))
-            {
-                socialMediaLinks.Select(x => new a(Href(x.Link))
-                {
-                    DisplayFlex, FlexDirectionRow, AlignItemsCenter, JustifyContentCenter,
-                    Border(Solid(1, Theme.grey_50)),
-                    BorderRadius,
-                    Padding(7),
-                    Transition(BackgroundColor, 200, cubic_bezier(0.4, 0, 0.2, 1), 0),
-                    Hover(Border(Solid(1, Theme.grey_300)), Background(Theme.grey_50)),
-
-                    new Tooltip
-                    {
-                        arrow = true,
-                        title = x.Text,
-
-                        children =
-                        {
-                            new svg(Size(20), svg.Fill(Theme.Blue700), svg.ViewBox(0, 0, 24, 24))
-                            {
-                                new path { d = x.Svg }
-                            }
-                        }
-                    }
-                })
             };
         }
     }
