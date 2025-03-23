d900c1655dfaa02254fab0f8914138c48f002c7a
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-03-23 17:05:47 +0300

    .

---
+++ ReactWithDotNet.WebSite/VisualDesigner/Views/StyleEditor.cs
@@ -0,0 +1,52 @@
+﻿namespace ReactWithDotNet.VisualDesigner.Views;
+
+sealed class StyleEditor : Component<StyleEditor.State>
+{
+    public IReadOnlyList<PropertyModel> Value { get; init; } = [new() { Name = "gap", Value = "5" }];
+
+    protected override Task constructor()
+    {
+        state = new()
+        {
+            InitialValue = Value ?? [],
+
+            Value = (Value ?? []).ToList()
+        };
+
+        return Task.CompletedTask;
+    }
+
+    protected override Element render()
+    {
+        return new FlexRow(AlignItemsCenter, FlexWrap, Border(1, solid, Gray300), BorderRadius(4), Padding(5, 10), Gap(16), Background(White))
+        {
+            EditorFontLinks,
+            EditorFont(),
+            
+            state.Value.Select(x => new PropertyEditor
+            {
+                Model               = x,
+                PropertySuggestions = StyleProperties
+            }),
+            new PropertyEditor
+            {
+                PropertySuggestions = StyleProperties,
+                OnChange            = OnAddNewItem
+            }
+        };
+    }
+
+    Task OnAddNewItem(PropertyModel newModel)
+    {
+        state.Value.Add(newModel);
+
+        return Task.CompletedTask;
+    }
+
+    internal class State
+    {
+        public IReadOnlyList<PropertyModel> InitialValue { get; init; }
+
+        public List<PropertyModel> Value { get; init; }
+    }
+}
---
+++ ReactWithDotNet.WebSite/VisualDesigner/Views/VisualElementTreeView.cs
@@ -0,0 +1,140 @@
+﻿namespace ReactWithDotNet.VisualDesigner.Views;
+
+sealed class VisualElementTreeView : Component<VisualElementTreeView.State>
+{
+    public VisualElementModel Model { get; init; }
+
+    public string SelectedPath { get; init; }
+
+    [CustomEvent]
+    public Func<string, Task> SelectionChanged { get; set; }
+
+    protected override Task constructor()
+    {
+        InitializeState();
+
+        return Task.CompletedTask;
+    }
+
+    protected override Element render()
+    {
+        return new div(CursorPointer, Padding(5), Border(Solid(1, rgb(217, 217, 217))), BorderRadius(3))
+        {
+            ToVisual(state.Model, 0, "0"),
+            WidthFull, HeightFull
+        };
+    }
+
+    void InitializeState()
+    {
+        state = new()
+        {
+            Model               = Model,
+            SelectedPath        = SelectedPath,
+            InitialModel        = Model,
+            InitialSelectedPath = SelectedPath
+        };
+    }
+
+    Task OnTreeItemClicked(MouseEvent e)
+    {
+        DispatchEvent(SelectionChanged, [e.currentTarget.id]);
+
+        return Task.CompletedTask;
+    }
+
+    IReadOnlyList<Element> ToVisual(VisualElementModel node, int indent, string path)
+    {
+        var isSelected = state.SelectedPath == path;
+
+        var returnList = new List<Element>
+        {
+            new FlexRow(AlignItemsCenter, PaddingLeft(indent * 16), Id(path), OnClick(OnTreeItemClicked))
+            {
+                new div { Text(node.Tag), MarginLeft(5), FontSize13 },
+
+                isSelected ? BackgroundImage(linear_gradient(90, rgb(136, 195, 242), rgb(242, 246, 249))) + BorderRadius(3) : null,
+
+                !isSelected ? Hover(BackgroundImage(linear_gradient(90, rgb(190, 220, 244), rgb(242, 246, 249))) + BorderRadius(3)) : null
+            }
+        };
+
+        if (node.HasChild is false)
+        {
+            return returnList;
+        }
+
+        for (var i = 0; i < node.Children.Count; i++)
+        {
+            var child = node.Children[i];
+
+            returnList.AddRange(ToVisual(child, indent + 1, $"{path},{i}"));
+        }
+
+        return returnList;
+    }
+
+    internal class State
+    {
+        public VisualElementModel InitialModel { get; init; }
+
+        public string InitialSelectedPath { get; set; }
+
+        public VisualElementModel Model { get; init; }
+
+        public string SelectedPath { get; set; }
+    }
+}
+
+sealed class DemoVisualElementTreeViewer : Component<DemoVisualElementTreeViewer.State>
+{
+    protected override Element render()
+    {
+        return new FlexColumn
+        {
+            new span { state.SelectedPath },
+            new VisualElementTreeView
+            {
+                SelectionChanged = SelectionChanged,
+                SelectedPath     = state.SelectedPath,
+                Model = new()
+                {
+                    Tag = "div",
+                    Children =
+                    [
+                        new() { Tag = "label", Text = "Abc1" },
+                        new() { Tag = "span", Text  = "Abc2" },
+                        new() { Tag = "ul", Text    = "Abc3" },
+
+                        new()
+                        {
+                            Tag = "div",
+                            Children =
+                            [
+                                new() { Tag = "label", Text = "Abc1" },
+                                new() { Tag = "span", Text  = "Abc2" },
+                                new()
+                                {
+                                    Tag  = "ul",
+                                    Text = "Abc3"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        };
+    }
+
+    Task SelectionChanged(string selectedPath)
+    {
+        state.SelectedPath = selectedPath;
+
+        return Task.CompletedTask;
+    }
+
+    internal class State
+    {
+        public string SelectedPath { get; set; }
+    }
+}
