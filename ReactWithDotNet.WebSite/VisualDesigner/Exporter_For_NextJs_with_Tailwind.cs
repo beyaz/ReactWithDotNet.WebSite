98af7ba329d335bc0b7b1d8287f466a5341c82f3
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-04-05 01:26:07 +0300

    .

--- ReactWithDotNet.WebSite/VisualDesigner/Exporter_For_NextJs_with_Tailwind.cs
+++ ReactWithDotNet.WebSite/VisualDesigner/Exporter_For_NextJs_with_Tailwind.cs
@@ -7,21 +7,46 @@ static class Exporter_For_NextJs_with_Tailwind
 {
     public static void Export(ApplicationState state)
     {
-        var code = GenerateHtml(state.ComponentRootElement);
+        var indentLevel = 1;
+        
+        var indent = new string(' ', indentLevel * 4);
 
-        File.WriteAllText($"C:\\github\\hopgogo\\web\\enduser-ui\\src\\components\\{state.ComponentName}.tsx", code);
+        var file = new StringBuilder();
+
+        file.AppendLine("import Link from \"next/link\";");
+        file.AppendLine();
+        
+        file.AppendLine($"export default function {state.ComponentName}() {{");
+
+        var partRender = GenerateHtml(state.ComponentRootElement, indentLevel);
+        
+        file.AppendLine($"{indent}return (");
+        file.AppendLine(partRender);
+        file.AppendLine($"{indent});");
+        
+        file.AppendLine("}");
+        
+        
+
+        File.WriteAllText($"C:\\github\\hopgogo\\web\\enduser-ui\\src\\components\\{state.ComponentName}.tsx", file.ToString());
     }
 
     public static string GenerateHtml(VisualElementModel element, int indentLevel = 0)
     {
-        var indent = new string(' ', indentLevel * 2);
+        var indent = new string(' ', indentLevel * 4);
 
         var sb = new StringBuilder();
 
         List<string> classNames = [];
 
         // Open tag
-        sb.Append($"{indent}<{element.Tag}");
+        var tag = element.Tag;
+        
+        if (tag == "a")
+        {
+            tag = "Link";
+        }
+        sb.Append($"{indent}<{tag}");
 
         // Add properties
         foreach (var property in element.Properties)
@@ -29,6 +54,12 @@ static class Exporter_For_NextJs_with_Tailwind
             var (success, name, value) = TryParsePropertyValue(property);
             if (success)
             {
+                if (name == "class")
+                {
+                    classNames.AddRange(value.Split(" ", StringSplitOptions.RemoveEmptyEntries));
+                    continue;
+                }
+                
                 sb.Append($" {name}=\"{value}\"");
             }
         }
@@ -40,11 +71,152 @@ static class Exporter_For_NextJs_with_Tailwind
                 var (success, name, value) = TryParsePropertyValue(styleItem);
                 if (success)
                 {
+                    if (name == "W" || name == "w" || name == "width")
+                    {
+                        if (value == "fit-content")
+                        {
+                            classNames.Add($"w-fit");
+                            continue;
+                        }
+                        classNames.Add($"w-[{value}px]");
+                        continue;
+                    }
+                    
+                    if (name == "h" || name == "height")
+                    {
+                        if (value == "fit-content")
+                        {
+                            classNames.Add($"h-fit");
+                            continue;
+                        }
+                        
+                        classNames.Add($"h-[{value}px]");
+                        continue;
+                    }
+                    
                     if (name == "pt")
                     {
                         classNames.Add($"pt-[{value}px]");
+                        continue;
                     }
+                    if (name == "pb")
+                    {
+                        classNames.Add($"pb-[{value}px]");
+                        continue;
+                    }
+                    
+                    if (name == "pl")
+                    {
+                        classNames.Add($"pl-[{value}px]");
+                        continue;
+                    }
+                    if (name == "pr")
+                    {
+                        classNames.Add($"pr-[{value}px]");
+                        continue;
+                    }
+                    
+                    if (name == "px")
+                    {
+                        classNames.Add($"px-[{value}px]");
+                        continue;
+                    }
+                    
+                    if (name == "py")
+                    {
+                        classNames.Add($"py-[{value}px]");
+                        continue;
+                    }
+                    
+                    if (name == "display")
+                    {
+                        classNames.Add($"{value}");
+                        continue;
+                    }
+                    
+                    if (name == "color")
+                    {
+                        if (Project.Colors.TryGetValue(value, out var htmlColor))
+                        {
+                            value = htmlColor;
+                        }
+                        classNames.Add($"text-[{value}]");
+                        continue;
+                    }
+                    
+                    if (name == "gap")
+                    {
+                        classNames.Add($"gap-[{value}px]");
+                        continue;
+                    }
+                    if (name == "flex-direction")
+                    {
+                        if (value == "column")
+                        {
+                            classNames.Add($"flex-col");
+                            continue;    
+                        }
+                        
+                    }
+                    
+                    if (name == "align-items")
+                    {
+                        classNames.Add($"items-{value.RemoveFromStart("align-")}");
+                        continue;
+                    }
+                    
+                    if (name == "justify-content")
+                    {
+                        if (value == "space-between")
+                        {
+                            classNames.Add("justify-between");
+                            continue;    
+                        }
+                        
+                    }
+                    
+                    if (name == "border-radius")
+                    {
+                        classNames.Add($"rounded-[{value}px]");
+                        continue;
+                    }
+                    
+                    if (name == "font-size")
+                    {
+                        classNames.Add($"[font-size:{value}px]");
+                        continue;
+                    }
+                    
+                    if (name == "border")
+                    {
+                        var parts = value.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                        if (parts.Length == 3)
+                        {
+                            if (Project.Colors.TryGetValue(parts[2], out var htmlColor))
+                            {
+                                parts[2] = htmlColor;
+                            }
+
+                            if (parts[0]=="1px" && parts[1] == "solid")
+                            {
+                                classNames.Add($"border");
+                                classNames.Add($"border-[{parts[2]}]"); 
+                                continue;
+                            }
+                            
+                            classNames.Add($"border-[{parts[0]}]");
+                            classNames.Add($"border-[{parts[1]}]");
+                            classNames.Add($"border-[{parts[2]}]"); 
+                            
+                            continue;
+                        }
+                        
+                        
+                    }
+                    
                 }
+                
+                classNames.Add(styleItem);
             }
         }
 
@@ -53,6 +225,12 @@ static class Exporter_For_NextJs_with_Tailwind
             sb.Append($" className=\"{string.Join(" ", classNames)}\"");
         }
 
+        var hasSelfClose = element.Children.Count == 0 && element.Text.HasNoValue();
+        if (hasSelfClose)
+        {
+            sb.AppendLine(" />"); 
+            return sb.ToString();
+        }
         sb.AppendLine(">");
 
         // Add text content
@@ -68,7 +246,7 @@ static class Exporter_For_NextJs_with_Tailwind
         }
 
         // Close tag
-        sb.AppendLine($"{indent}</{element.Tag}>");
+        sb.AppendLine($"{indent}</{tag}>");
 
         return sb.ToString();
     }
