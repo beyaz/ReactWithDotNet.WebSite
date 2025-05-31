7ea95e1357fcd5e198133d7e386ff7cce9608100
Abdullah Beyaztaş <beyaz1404@gmail.com>
2025-05-31 08:32:55 +0300

    .

--- ReactWithDotNet.WebSite/GlobalUsings.cs
+++ ReactWithDotNet.WebSite/GlobalUsings.cs
@@ -4,6 +4,7 @@ global using ReactWithDotNet;
 global using System.Collections.Generic;
 global using System.Linq;
 global using static ReactWithDotNet.Mixin;
+global using static ReactWithDotNet.Tailwind;
 global using static ReactWithDotNet.WebSite.Extensions;
 global using ReactWithDotNet.WebSite.Components;
 global using System.Threading.Tasks;
